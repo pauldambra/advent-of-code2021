46faae0cd8b66209ae507430bfde9e5c5fda03ac
Paul D'Ambra <paul.dambra@gmail.com>
2021-12-01 20:18:58 +0000

    quqlity of life fangling

--- DAY_ONE/test_sonar.py
+++ DAY_ONE/test_sonar.py
@@ -1,5 +1,17 @@
 from itertools import islice, tee
 from typing import Optional, Iterator
+from unittest import TestCase
+
+
+def sliding_windows(sequence: Iterator, window_size: int = 3) -> list:
+    iterables = tee(iter(sequence), window_size)
+    window = zip(*(islice(t, n, None) for n, t in enumerate(iterables)))
+    yield from window
+
+
+def as_integers(ss: Iterator[str]) -> Iterator[int]:
+    for s in ss:
+        yield int(s)
 
 
 def check_sonar_readings_for_increases(sonar_readings: Iterator[str]) -> int:
@@ -26,42 +38,45 @@ example_sonar_readings = """199
 263"""
 
 
-def test_example_sonar():
-    increases = check_sonar_readings_for_increases(iter(example_sonar_readings.splitlines()))
-
-    assert increases == 7
-
-
-def test_file_input():
-    with open('part1.input', 'r', newline='\n') as f:
-        increases = check_sonar_readings_for_increases(iter(f.readlines()))
-
-        assert increases == 1374
-
-
-def sliding_windows(sequence: Iterator, window_size: int = 3) -> list:
-    iterables = tee(iter(sequence), window_size)
-    window = zip(*(islice(t, n, None) for n, t in enumerate(iterables)))
-    yield from window
-
-
-def test_read_sequence_in_threes():
-    sequence = [0, 1, 2, 3, 4, 5]
-    assert [x for x in sliding_windows(sequence)] == [(0, 1, 2), (1, 2, 3), (2, 3, 4), (3, 4, 5)]
-
-
-def test_sonar_example_sliding_window():
-    increases = check_sonar_readings_for_increases(
-        [sum(w) for w in sliding_windows([int(s) for s in iter(example_sonar_readings.splitlines())])]
-    )
-
-    assert increases == 5
-
-
-def test_file_input_in_windows():
-    with open('part1.input', 'r', newline='\n') as f:
+class DayOne(TestCase):
+    def test_example_sonar(self):
         increases = check_sonar_readings_for_increases(
-            [sum(w) for w in sliding_windows([int(s) for s in iter(f.readlines())])]
+            iter(example_sonar_readings.splitlines())
         )
 
-        assert increases == 1418
+        assert increases == 7
+
+    def test_file_input(self):
+        with open("part1.input", "r", newline="\n") as f:
+            increases = check_sonar_readings_for_increases(iter(f.readlines()))
+
+            assert increases == 1374
+
+    def test_read_sequence_in_threes(self):
+        sequence = [0, 1, 2, 3, 4, 5]
+        assert [x for x in sliding_windows(sequence)] == [
+            (0, 1, 2),
+            (1, 2, 3),
+            (2, 3, 4),
+            (3, 4, 5),
+        ]
+
+    def test_sonar_example_sliding_window(self):
+        increases = check_sonar_readings_for_increases(
+            [
+                sum(w)
+                for w in sliding_windows(
+                    as_integers(iter(example_sonar_readings.splitlines()))
+                )
+            ]
+        )
+
+        assert increases == 5
+
+    def test_file_input_in_windows(self):
+        with open("part1.input", "r", newline="\n") as f:
+            increases = check_sonar_readings_for_increases(
+                [sum(w) for w in sliding_windows(as_integers(iter(f.readlines())))]
+            )
+
+            assert increases == 1418
---
+++ README.md
@@ -0,0 +1,4 @@
+# Intalling dependencies
+
+* Add them to requirements.in
+* `pip-compile requirements.in && pip install -r requirements.txt`
