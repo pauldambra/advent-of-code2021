a5de4f1c5d50f9327f88fdb05a16692c053834d7
Paul D'Ambra <paul.dambra@gmail.com>
2021-12-04 15:44:34 +0000

    day 3 part 2

--- DAY_THREE/test_diagnostics.py
+++ DAY_THREE/test_diagnostics.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Iterator
+from typing import Iterator, Callable
 from unittest import TestCase
 
 puzzle_input_path = Path(__file__).parent / "./puzzle.input"
@@ -34,7 +34,7 @@ def as_columns(rows: Iterator[str]) -> Iterator[list[int]]:
 def count_bits(columns: Iterator[list[int]]) -> dict[int, dict[int]]:
     grouped = {}
     for index, column in enumerate(columns):
-        grouped[index] = {}
+        grouped[index] = {0: 0, 1: 0}
         for bit in column:
             if bit in grouped[index]:
                 grouped[index][bit] += 1
@@ -48,6 +48,15 @@ def to_most_common_bits(counted_bits: dict[int, dict[int]]) -> list[int]:
     rates = []
     for column_count in counted_bits.values():
         rates.append(0 if column_count[0] > column_count[1] else 1)
+
+    return rates
+
+
+def to_least_common_bits(counted_bits: dict[int, dict[int]]) -> list[int]:
+    rates = []
+    for column_count in counted_bits.values():
+        rates.append(1 if column_count[0] > column_count[1] else 0)
+
     return rates
 
 
@@ -57,6 +66,34 @@ def as_epsilon(bits: list[int]) -> list[int]:
 
 def to_number(bits):
     return int("".join([str(i) for i in bits]), 2)
+
+
+def get_rating(
+    diagnostic_input: list[str],
+    bit_comparison: Callable[[dict[int, dict[int]]], list[int]],
+) -> int:
+    candidates = diagnostic_input
+
+    column_index = 0
+    while len(candidates) != 1 and column_index <= len(candidates[0]):
+        compared = bit_comparison(count_bits(as_columns(iter(candidates))))
+
+        candidates = [
+            c
+            for c in candidates
+            if list(c)[column_index] == str(compared[column_index])
+        ]
+        column_index += 1
+
+    return to_number(candidates[0])
+
+
+def get_o2_rating(diagnostic_input: list[str]) -> int:
+    return get_rating(diagnostic_input, to_most_common_bits)
+
+
+def get_co2_scrubber_rating(diagnostic_input: list[str]) -> int:
+    return get_rating(diagnostic_input, to_least_common_bits)
 
 
 class TestDiagnotics(TestCase):
@@ -91,3 +128,36 @@ class TestDiagnotics(TestCase):
             epsilon = to_number(as_epsilon(bits))
             power_consumption = gamma * epsilon
             assert power_consumption == 3847100
+
+    def test_most_common_is_one_if_equal_number_of_bits(self):
+        most_common_bits = to_most_common_bits(count_bits(as_columns(iter(["0", "1"]))))
+        assert most_common_bits == [1]
+
+    def test_most_common_bits_for_known_buggy_example(self):
+        rows = ["11110", "11100", "11001"]
+        counted_bits = count_bits(as_columns(iter(rows)))
+        most_common_bits = to_most_common_bits(counted_bits)
+        assert most_common_bits == [1, 1, 1, 0, 0]
+
+    def test_counted_bits_always_has_zero_and_one(self):
+        rows = ["10", "10", "10"]
+        counted_bits = count_bits(as_columns(iter(rows)))
+        assert {0: {0: 0, 1: 3}, 1: {0: 3, 1: 0}} == counted_bits
+
+    def test_oxygen_rating(self):
+        rating = get_o2_rating(example_input.splitlines())
+
+        assert rating == 23
+
+    def test_co2_scrubber_rating(self):
+        rating = get_co2_scrubber_rating(example_input.splitlines())
+
+        assert rating == 10
+
+    def test_solve_part_two(self):
+        with open(puzzle_input_path, "r", newline="\n") as f:
+            lines = f.readlines()
+            o2_rating = get_o2_rating(lines)
+            co2_rating = get_co2_scrubber_rating(lines)
+
+            assert o2_rating * co2_rating == 4105235
