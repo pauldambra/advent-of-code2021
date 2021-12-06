03e661131c1face20a6dafe411e997b978452f49
Paul D'Ambra <paul.dambra@gmail.com>
2021-12-06 15:01:26 +0000

    day 2 part 2

--- DAY_SIX/test_lanternfish.py
+++ DAY_SIX/test_lanternfish.py
@@ -304,91 +304,155 @@ puzzle_input = [
 ]
 
 
-# from https://stackoverflow.com/a/9671301/222163
-def grouper(iterable: list[int], n: int = 500) -> list[list[int]]:
-    return [iterable[x : x + n] for x in range(0, len(iterable), n)]
+def parse_list_to_dict(ns: list[int]) -> dict[int, int]:
+    result = {}
+    for n in ns:
+        if n not in result:
+            result[n] = 0
+        result[n] += 1
+
+    return result
 
 
-fish_fridge: dict[tuple[int], tuple[list[int], int]] = {}
+example_input = {1: 1, 2: 1, 3: 2, 4: 1}
 
 
-def tick(fish: list[int]) -> list[int]:
-    next_tick = []
-    new_fish = 0
-    for group in grouper(fish):
-        if tuple(group) not in fish_fridge:
-            chunk = []
-            new_fish_chunk = 0
-            for f in group:
-                if f == 0:
-                    chunk.append(6)
-                    new_fish_chunk += 1
-                elif f:
-                    chunk.append(f - 1)
+def tick(fish: dict[int, int], times: int) -> dict[int, int]:
+    current = fish
+    for x in range(1, times + 1):
+        next_fish = {}
+        for n in range(8, -1, -1):
+            if n != 0:
+                next_fish[n - 1] = current.get(n, 0)
+            else:
+                birthing = current.get(0, 0)
+                next_fish[6] += birthing
+                next_fish[8] = next_fish.get(8, 0) + birthing
 
-            fish_fridge[tuple(group)] = (chunk, new_fish_chunk)
-        else:
-            (chunk, new_fish_chunk) = fish_fridge[tuple(group)]
+        current = next_fish
 
-        next_tick += chunk
-        new_fish += new_fish_chunk
+    return current
 
-    return next_tick + [8] * new_fish
+
+# def tick(fish: list[int]) -> list[int]:
+#     next_tick = []
+#     new_fish = 0
+#     for group in grouper(fish):
+#         if tuple(group) not in fish_fridge:
+#             chunk = []
+#             new_fish_chunk = 0
+#             for f in group:
+#                 if f == 0:
+#                     chunk.append(6)
+#                     new_fish_chunk += 1
+#                 elif f:
+#                     chunk.append(f - 1)
+#
+#             fish_fridge[tuple(group)] = (chunk, new_fish_chunk)
+#         else:
+#             (chunk, new_fish_chunk) = fish_fridge[tuple(group)]
+#
+#         next_tick += chunk
+#         new_fish += new_fish_chunk
+#
+#     return next_tick + [8] * new_fish
 
 
 class TestLanternFish(TestCase):
-    def test_single_fish(self):
-        fish = [3]
-        fish = tick(fish)
-        assert fish == [2]
+    def test_single_fish_one_tick(self):
+        fish = tick({3: 1}, times=1)
+        assert fish == {0: 0, 1: 0, 2: 1, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
 
-        fish = tick(tick(fish))
-        assert fish == [0]
+    def test_single_fish_three_tick(self):
+        fish = tick({3: 1}, times=3)
+        assert fish == {0: 1, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
 
-        fish = tick(fish)
-        assert fish == [6, 8]
+    def test_single_fish_four_ticks(self):
+        fish = tick({3: 1}, times=4)
+        assert fish == {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 1, 7: 0, 8: 1}
 
-    def test_a_list_of_fish(self):
-        fish = [3, 4, 3, 1, 2]
-        fish = tick(tick(fish))  # after 2 days
+    def test_example_two_ticks(self):
+        fish = tick(example_input, times=2)  # after 2 days
 
-        assert fish == [1, 2, 1, 6, 0, 8]
+        assert fish == {0: 1, 1: 2, 2: 1, 3: 0, 4: 0, 5: 0, 6: 1, 7: 0, 8: 1}
 
-        fish = tick(tick(fish))
-        assert fish == [6, 0, 6, 4, 5, 6, 7, 8, 8]  # after 4 days
+    def test_example_four_ticks(self):
+        fish = tick(example_input, times=4)
+        assert fish == {
+            0: 1,
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 1,
+            5: 1,
+            6: 3,
+            7: 1,
+            8: 2,
+        }  # after 4 days 6,0,6,4,5,6,7,8,8
 
-        fish = tick(tick(fish))
-        assert fish == [4, 5, 4, 2, 3, 4, 5, 6, 6, 7]  # after 6 days
+    def test_example_six_ticks(self):
+        fish = tick(example_input, times=6)
+        assert fish == {
+            0: 0,
+            1: 0,
+            2: 1,
+            3: 1,
+            4: 3,
+            5: 2,
+            6: 2,
+            7: 1,
+            8: 0,
+        }  # after 6 days
 
-        fish = tick(tick(fish))
-        assert fish == [2, 3, 2, 0, 1, 2, 3, 4, 4, 5]  # after 8 days
+    def test_example_eight_ticks(self):
+        fish = tick(example_input, times=8)
+        assert fish == {
+            0: 1,
+            1: 1,
+            2: 3,
+            3: 2,
+            4: 2,
+            5: 1,
+            6: 0,
+            7: 0,
+            8: 0,
+        }  # after 8 days
 
-        fish = tick(tick(fish))
-        assert fish == [0, 1, 0, 5, 6, 0, 1, 2, 2, 3, 7, 8]  # after 10 days
+    def test_example_ten_ticks(self):
+        fish = tick(example_input, times=10)
+        assert fish == {
+            0: 3,
+            1: 2,
+            2: 2,
+            3: 1,
+            4: 0,
+            5: 1,
+            6: 1,
+            7: 1,
+            8: 1,
+        }  # after 10 days
 
-        fish = [3, 4, 3, 1, 2]
-        for n in range(0, 18):
-            fish = tick(fish)
+    def test_example_eighteen_ticks(self):
+        fish = tick(example_input, times=18)
 
-        assert len(fish) == 26
+        assert sum(fish.values()) == 26
 
-        fish = [3, 4, 3, 1, 2]
-        for n in range(0, 80):
-            fish = tick(fish)
+    def test_example_eighty_ticks(self):
+        fish = tick(example_input, times=80)
 
-        assert len(fish) == 5934
+        assert sum(fish.values()) == 5934
 
     def test_puzzle_input(self):
-        fish = puzzle_input
+        fish = tick(parse_list_to_dict(puzzle_input), times=80)
 
-        for n in range(0, 80):
-            fish = tick(fish)
-
-        assert len(fish) == 362346
+        assert sum(fish.values()) == 362346
 
     def test_example_part_two(self):
-        fish = [3, 4, 3, 1, 2]
-        for n in range(0, 256):
-            fish = tick(fish)
+        fish = tick(example_input, times=256)
 
-        assert len(fish) == 26984457539
+        assert sum(fish.values()) == 26984457539
+
+    def test_puzzle_input_part_two(self):
+        fish = tick(parse_list_to_dict(puzzle_input), times=256)
+
+        assert sum(fish.values()) == 1639643057051
