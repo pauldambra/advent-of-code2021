768ed92bf25506fedc2fa5e9267fea1fc169bc5c
Paul D'Ambra <paul.dambra@gmail.com>
2021-12-12 21:06:59 +0000

    day 12 part 2

--- DAY_TWELVE/test_paths.py
+++ DAY_TWELVE/test_paths.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from unittest import TestCase
 
 
@@ -22,11 +23,7 @@ class CaveSystem:
             self.links[start].append(end)
             self.links[end].append(start)
 
-        print(self.links)
-
         self.paths = self.explore_caves("start", "end", [])
-
-        print(self.paths)
 
     def explore_caves(self, start: str, end: str, path: list[str]) -> list[list[str]]:
         path = path + [start]
@@ -41,6 +38,56 @@ class CaveSystem:
                     paths.append(new_path)
 
         return paths
+
+
+class CaveSystemPartTwo:
+    def __init__(self, cave_description: str):
+        self.paths = []
+        self.cave_description = cave_description
+        self.visited = {}
+        self.links: dict[str, list[str]] = {}
+
+        for p in self.cave_description.splitlines():
+            start: str
+            end: str
+            start, end = p.split("-")
+            start = start.strip()
+            end = end.strip()
+            if start not in self.links:
+                self.links[start] = []
+            if end not in self.links:
+                self.links[end] = []
+
+            self.links[start].append(end)
+            self.links[end].append(start)
+
+        self.paths = self.explore_caves("start", "end", [])
+
+    def explore_caves(self, start: str, end: str, path: list[str]) -> list[list[str]]:
+        path = path + [start]
+        if start == end:
+            return [path]
+
+        paths = []
+        for node in self.links.get(start, []):
+            if self.can_visit(node, path):
+                new_paths = self.explore_caves(node, end, path)
+                for new_path in new_paths:
+                    paths.append(new_path)
+
+        return paths
+
+    def can_visit(self, node, path):
+        isupper = node.isupper()
+        not_in_path = node not in path
+        not_in_start_end_ = node not in ["start", "end"]
+        nothing_in_path_twice = (
+            max(Counter([p for p in path if p.islower()]).values()) < 2
+        )
+        can_visit = (
+            isupper or not_in_path or (not_in_start_end_ and nothing_in_path_twice)
+        )
+        return can_visit
 
 
 class TestPaths(TestCase):
@@ -126,23 +173,23 @@ kj-dc"""
             len(
                 CaveSystem(
                     """fs-end
-            he-DX
-            fs-he
-            start-DX
-            pj-DX
-            end-zg
-            zg-sl
-            zg-pj
-            pj-he
-            RW-he
-            fs-DX
-            pj-RW
-            zg-RW
-            start-pj
-            he-WI
-            zg-he
-            pj-fs
-            start-RW"""
+                                                                                he-DX
+                                                                                fs-he
+                                                                                start-DX
+                                                                                pj-DX
+                                                                                end-zg
+                                                                                zg-sl
+                                                                                zg-pj
+                                                                                pj-he
+                                                                                RW-he
+                                                                                fs-DX
+                                                                                pj-RW
+                                                                                zg-RW
+                                                                                start-pj
+                                                                                he-WI
+                                                                                zg-he
+                                                                                pj-fs
+                                                                                start-RW"""
                 ).paths
             )
             == 226
@@ -153,30 +200,112 @@ kj-dc"""
             len(
                 CaveSystem(
                     """start-qs
-    qs-jz
-    start-lm
-    qb-QV
-    QV-dr
-    QV-end
-    ni-qb
-    VH-jz
-    qs-lm
-    qb-end
-    dr-fu
-    jz-lm
-    start-VH
-    QV-jz
-    VH-qs
-    lm-dr
-    dr-ni
-    ni-jz
-    lm-QV
-    jz-dr
-    ni-end
-    VH-dr
-    VH-ni
-    qb-HE"""
+                                                                        qs-jz
+                                                                        start-lm
+                                                                        qb-QV
+                                                                        QV-dr
+                                                                        QV-end
+                                                                        ni-qb
+                                                                        VH-jz
+                                                                        qs-lm
+                                                                        qb-end
+                                                                        dr-fu
+                                                                        jz-lm
+                                                                        start-VH
+                                                                        QV-jz
+                                                                        VH-qs
+                                                                        lm-dr
+                                                                        dr-ni
+                                                                        ni-jz
+                                                                        lm-QV
+                                                                        jz-dr
+                                                                        ni-end
+                                                                        VH-dr
+                                                                        VH-ni
+                                                                        qb-HE"""
                 ).paths
             )
             == 5178
         )
+
+    def test_example_paths_part_two(self):
+        cave_system = CaveSystemPartTwo(
+            """start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"""
+        )
+
+        assert sorted(cave_system.paths) == sorted(
+            [
+                ["start", "A", "b", "A", "b", "A", "c", "A", "end"],
+                ["start", "A", "b", "A", "b", "A", "end"],
+                ["start", "A", "b", "A", "b", "end"],
+                ["start", "A", "b", "A", "c", "A", "b", "A", "end"],
+                ["start", "A", "b", "A", "c", "A", "b", "end"],
+                ["start", "A", "b", "A", "c", "A", "c", "A", "end"],
+                ["start", "A", "b", "A", "c", "A", "end"],
+                ["start", "A", "b", "A", "end"],
+                ["start", "A", "b", "d", "b", "A", "c", "A", "end"],
+                ["start", "A", "b", "d", "b", "A", "end"],
+                ["start", "A", "b", "d", "b", "end"],
+                ["start", "A", "b", "end"],
+                ["start", "A", "c", "A", "b", "A", "b", "A", "end"],
+                ["start", "A", "c", "A", "b", "A", "b", "end"],
+                ["start", "A", "c", "A", "b", "A", "c", "A", "end"],
+                ["start", "A", "c", "A", "b", "A", "end"],
+                ["start", "A", "c", "A", "b", "d", "b", "A", "end"],
+                ["start", "A", "c", "A", "b", "d", "b", "end"],
+                ["start", "A", "c", "A", "b", "end"],
+                ["start", "A", "c", "A", "c", "A", "b", "A", "end"],
+                ["start", "A", "c", "A", "c", "A", "b", "end"],
+                ["start", "A", "c", "A", "c", "A", "end"],
+                ["start", "A", "c", "A", "end"],
+                ["start", "A", "end"],
+                ["start", "b", "A", "b", "A", "c", "A", "end"],
+                ["start", "b", "A", "b", "A", "end"],
+                ["start", "b", "A", "b", "end"],
+                ["start", "b", "A", "c", "A", "b", "A", "end"],
+                ["start", "b", "A", "c", "A", "b", "end"],
+                ["start", "b", "A", "c", "A", "c", "A", "end"],
+                ["start", "b", "A", "c", "A", "end"],
+                ["start", "b", "A", "end"],
+                ["start", "b", "d", "b", "A", "c", "A", "end"],
+                ["start", "b", "d", "b", "A", "end"],
+                ["start", "b", "d", "b", "end"],
+                ["start", "b", "end"],
+            ]
+        )
+
+    def test_puzzle_input_paths_part_two(self):
+        cave_system = CaveSystemPartTwo(
+            """start-qs
+                                                        qs-jz
+                                                        start-lm
+                                                        qb-QV
+                                                        QV-dr
+                                                        QV-end
+                                                        ni-qb
+                                                        VH-jz
+                                                        qs-lm
+                                                        qb-end
+                                                        dr-fu
+                                                        jz-lm
+                                                        start-VH
+                                                        QV-jz
+                                                        VH-qs
+                                                        lm-dr
+                                                        dr-ni
+                                                        ni-jz
+                                                        lm-QV
+                                                        jz-dr
+                                                        ni-end
+                                                        VH-dr
+                                                        VH-ni
+                                                        qb-HE"""
+        )
+
+        assert len(cave_system.paths) == 130094
